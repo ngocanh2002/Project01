5f37d335da270ed6bd33dc45d117225838c34df1
unknown <phnanh46@gmail.com>
2021-09-14 11:44:37 +0700

    C2 - Added App file

---
+++ App/app.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: '127.0.0.1',
+  user: "developer",
+  password: "abc@123456",
+  database: "ddv_vmg_tester"
+});
+
+con.connect(function (err){
+    
+    if (err) throw err.stack;
+  
+    const sql = "INSERT INTO Product(ID, Name, Price) values('8','Iphone13','40000000')";
+    const select_record= "SELECT *FROM Product";
+
+    // 1/ Select products which name is 'Iphone'
+    const task_1 = "SELECT * FROM Product WHERE Name = 'Iphone'";
+
+    // 2/ Select a list of products which price is from 15M to 30M. And order that list by ascending and descending
+    const task_2 = "SELECT * FROM Product where price >= 15000000 AND price <= 30000000 ORDER BY Price DESC";
+    // 3/ Search products which has 'No'
+    const task_3 = "SELECT * FROM Product where Name like 'No%'";
+
+    con.query(task_3, function (err,results) {
+        if (err) throw err;
+        console.log(results);
+    });
+});
+
+
+
